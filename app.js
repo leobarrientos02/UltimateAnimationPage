96b7aa3cc9b74ad92516371f3db40910409a9487
Your Name <leobarrientos02@gmail.com>
2022-01-20 21:46:54 -0500

    another animation

--- app.js
+++ app.js
@@ -72,3 +72,69 @@ tlHRemove.to(".highlight", {
   color: "rgba(255, 255, 255, 0.4)",
   stagger: 1,
 });
+
+//Page 3
+const tlSplit = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".third-page",
+    start: "-30%",
+    end: "20%",
+    markers: true,
+    scrub: true,
+  },
+});
+
+tlSplit.fromTo(
+  ".large-phone",
+  {
+    x: "40%",
+  },
+  {
+    x: "20%",
+  }
+);
+tlSplit.fromTo(
+  ".small-phone",
+  {
+    x: "-40%",
+  },
+  {
+    x: "-20%",
+  },
+  "<"
+);
+
+tlSplit.fromTo(
+  ".product-text-left",
+  {
+    x: 50,
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    x: 0,
+  },
+  "<"
+);
+tlSplit.fromTo(
+  ".product-text-right",
+  {
+    x: -50,
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    x: 0,
+  },
+  "<"
+);
+
+const tlSplitPin = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".third-page",
+    pin: true,
+    pinSpacing: false,
+    start: "0%",
+    end: "100%",
+  },
+});
