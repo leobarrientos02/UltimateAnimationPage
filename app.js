28d5156f370ff4bfbe8897eb965e65e71f8ffa7d
Your Name <leobarrientos02@gmail.com>
2022-01-20 21:24:10 -0500

    Scroll Trigger animations on text

--- app.js
+++ app.js
@@ -1,20 +1,74 @@
 // SCROLL TRIGGER
+// const tlIntro = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".first-page",
+//     start: "0%",
+//     end: "25%",
+//     //markers: true,
+//     scrub: true,
+//   },
+// });
+
+// tlIntro.fromTo(
+//   "nav",
+//   {
+//     opacity: 1,
+//   },
+//   {
+//     opacity: 0,
+//   }
+// );
+
+// Pin the first page
 const tlIntro = gsap.timeline({
   scrollTrigger: {
     trigger: ".first-page",
     start: "0%",
-    end: "25%",
-    //markers: true,
-    scrub: true,
+    end: "100%",
+    pin: true,
+    pinSpacing: false,
   },
 });
 
-tlIntro.fromTo(
-  "nav",
+// Highlight page2
+const tlH = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".second-page",
+    markers: {
+      startColor: "blue",
+      endColor: "blue",
+    },
+    scrub: true,
+    start: "-40%",
+    end: "40%",
+  },
+});
+
+tlH.fromTo(
+  ".highlight",
   {
-    opacity: 1,
+    color: "rgba(255, 255, 255, 0.4)",
   },
   {
-    opacity: 0,
+    color: "rgba(255, 255, 255, 1.0)",
+    stagger: 1,
   }
 );
+
+const tlHRemove = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".second-page",
+    markers: {
+      startColor: "pink",
+      endColor: "pink",
+    },
+    scrub: true,
+    start: "-20%",
+    end: "60%",
+  },
+});
+
+tlHRemove.to(".highlight", {
+  color: "rgba(255, 255, 255, 0.4)",
+  stagger: 1,
+});
